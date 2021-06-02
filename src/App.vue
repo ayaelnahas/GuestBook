e6c3a8e6e0e0f1ea5cbdb4b2d0a6f150309801fd
Aya ElNahas <ayaa.alnahas@gmail.com>
2021-06-02 23:13:54 +0200

    created login form

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <v-app>
     <NavBar />
-    <MessageItem />
     <router-view />
   </v-app>
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
-import MessageItem from "./components/MessageItem.vue";
 export default {
   name: "App",
   components: {
     NavBar,
-    MessageItem,
   },
 
   data: () => ({
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,37 +1,71 @@
-<template lang="html">
-
-  <section class="login-form">
-    <h1>login-form Component</h1>
-  </section>
-
+<template>
+  <div class="loginForm">
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-container>
+        <v-row v-row justify="center">
+          <v-col cols="12">
+            <v-text-field
+              filled
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-text-field
+              filled
+              v-model="password"
+              :rules="[rules.required, rules.min]"
+              :type="show1 ? 'text' : 'password'"
+              name="input-10-1"
+              hint="At least 8 characters"
+              label="password"
+              counter
+              @click:append="show1 = !show1"
+            ></v-text-field>
+          </v-col>
+          <v-btn
+            :disabled="!valid"
+            color="primary"
+            class="mr-4"
+            @click="validate"
+          >
+            Log In
+          </v-btn>
+        </v-row>
+      </v-container>
+    </v-form>
+  </div>
 </template>
 
-<script lang="js">
-
-  export default  {
-    name: 'login-form',
-    props: [],
-    mounted () {
-
+<script>
+export default {
+  name: "login-form",
+  props: [],
+  mounted() {},
+  data() {
+    return {
+      show1: false,
+      valid: true,
+      password: "",
+      email: "",
+      emailRules: [
+        (v) => !!v || "E-mail is required",
+        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+      ],
+      rules: {
+        required: (value) => !!value || "Required.",
+        min: (v) => v.length >= 8 || "Min 8 characters",
+        emailMatch: () => `The email and password you entered don't match`,
+      },
+    };
+  },
+  methods: {
+    validate() {
+      this.$refs.form.validate();
     },
-    data () {
-      return {
-
-      }
-    },
-    methods: {
-
-    },
-    computed: {
-
-    }
-}
-
-
+  },
+  computed: {},
+};
 </script>
-
-<style scoped lang="scss">
-  .login-form {
-
-  }
-</style>
